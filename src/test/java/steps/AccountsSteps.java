3ea46005df9465e60d6baf6380b61dfef1800fda
Lena Gorelik <lenagorelik01@gmail.com>
2022-02-17 16:10:22 +0100

    added builder

--- src/test/java/steps/AccountsSteps.java
+++ src/test/java/steps/AccountsSteps.java
@@ -15,7 +15,7 @@ public class AccountsSteps extends AbstractStep {
         super(driver);
     }
 
-    public AccountsSteps createNewAccount() {
+    public AccountsSteps createNewAccount(AccountModel accountModel) {
         accountPage = new AccountPage(driver);
         accountPage.openNewAccountForm();
         CreateAccountFormComponent form = new CreateAccountFormComponent(driver);
@@ -23,31 +23,53 @@ public class AccountsSteps extends AbstractStep {
                 form.isComponentDisplayed(),
                 form.getClass().getSimpleName().concat(" not displayed")
         );
-        Input accountNameInput = new Input(driver, "Account Name");
-        accountNameInput.insert("USER2");
-
-        Input phoneInput = new Input(driver, "Phone");
-        phoneInput.insert("375295340651");
-
-        new Input(driver, "Fax").insert("375292780448");
-        new Input(driver, "Website").insert("teachmeskills.by");
-        new Dropdown(driver, "Type").selectOption("Customer");
-        new Input(driver, "Employees").insert("10");
-        new Dropdown(driver, "Industry").selectOption("Banking");
-        new Input(driver, "Annual Revenue").insert("1200");
-        new TextArea(driver, "Description").write("Мы используем файлы cookie, " +
-                "чтобы иметь возможность предлагать Вам полезные функции");
-        new TextArea(driver, "Billing Street").write("Kosmonawtow");
-        new Input(driver, "Billing City").insert("Minsk");
-        new Input(driver, "Billing Zip/Postal Code").insert("220107");
-        new Input(driver, "Billing Country").insert("Belarus");
-        new Checkbox(driver).clickCheckBox();
+        fillAccountForm(accountModel);
+        //new Checkbox(driver).clickCheckBox();
         form.save();
         validatePageIsLoaded(accountPage);
         return this;
     }
 
-    public void validateAccountCreated() {
+    private void fillAccountForm(AccountModel accountModel) {
+        new Input(driver, "Account Name").insert(accountModel.getAccountName());
+        new Input(driver, "Phone").insert(accountModel.getPhone());
+        new Input(driver, "Fax").insert(accountModel.getFax());
+        new Input(driver, "Website").insert(accountModel.getWebsite());
+        new Dropdown(driver, "Type").selectOption(accountModel.getType());
+        new Input(driver, "Employees").insert(String.valueOf(accountModel.getEmployees()));
+        new Dropdown(driver, "Industry").selectOption(accountModel.getIndustry());
+        //new Input(driver, "Annual Revenue").insert(String.valueOf(accountModel.getAnnualRevenue()));
+        new TextArea(driver, "Description").write(accountModel.getDescription());
+        //new TextArea(driver, "Billing Street").write(accountModel.getBillingStreet());
+        //new Input(driver, "Billing City").insert(accountModel.getBillingCity());
+        //new Input(driver, "Billing Zip/Postal Code").insert(accountModel.getBillingCode());
+        //new Input(driver, "Billing Country").insert(accountModel.getBillingCountry());
+    }
+
+    public void validateAccountCreated(AccountModel expectedModel) {
+        accountPage.openDetails();
+        AccountModel actualModel = getActualAccount();
+        Assert.assertEquals(actualModel, expectedModel,
+                String.format("Account is not valid: %s", actualModel));
+    }
+
+    private AccountModel getActualAccount() {
+        AccountModel actualModel = new AccountModel();
+        actualModel.setAccountName(new AccountDetailsComponent(driver, "Account Name", "text").getValue());
+        actualModel.setPhone(new AccountDetailsComponent(driver, "Phone", "phone").getValue());
+        actualModel.setFax(new AccountDetailsComponent(driver, "Fax", "phone").getValue());
+        actualModel.setWebsite(new AccountDetailsComponent(driver, "Website", "url").getValue());
+        actualModel.setType(new AccountDetailsComponent(driver, "Type", "text").getValue());
+        actualModel.setEmployees(Integer.parseInt(new AccountDetailsComponent(driver, "Employees", "number").getValue()));
+        actualModel.setIndustry(new AccountDetailsComponent(driver, "Industry", "text").getValue());
+        //actualModel.setAnnualRevenue(new AccountDetailsComponent(driver, "Annual Revenue", "text").getValue());
+        actualModel.setDescription(new AccountDetailsComponent(driver, "Description", "text").getValue());
+        //actualModel.setBillingAddress(new AccountDetailsComponent(driver, "Billing Address", "address").getValue());
+        //actualModel.setShippingAddress(new AccountDetailsComponent(driver, "Shipping Address", "address").getValue());
+        return actualModel;
+    }
+
+    public void validateAccountCreated_DEL() {
         accountPage.openDetails();
         Assert.assertEquals(new AccountDetailsComponent(driver,
                 "Account Name", "text").getValue(), "USER2");
@@ -72,80 +94,6 @@ public class AccountsSteps extends AbstractStep {
                 "Billing Address", "address").getValue(), "Kosmonawtow\nMinsk, 220107\nBelarus");
         Assert.assertEquals(new AccountDetailsComponent(driver,
                 "Shipping Address", "address").getValue(), "Kosmonawtow\nMinsk, 220107\nBelarus");
-    }
-
-    /*public void validateAccountCreated_DEL() {
-        accountPage.openDetails();
-        Assert.assertEquals(
-                new AccountDetailsComponent(driver, "Account Name", "text").getValue(),
-                "USERNAME"
-        );
-        Assert.assertEquals(
-                new AccountDetailsComponent(driver, "Phone", "phone").getValue(),
-                "+37529123123"
-        );
-        Assert.assertEquals(
-                new AccountDetailsComponent(driver, "Website", "url").getValue(),
-                "teachmeskills.by"
-        );
-        Assert.assertEquals(
-                new AccountDetailsComponent(driver, "Employees", "number").getValue(),
-                "10"
-        );
-        Assert.assertEquals(
-                new AccountDetailsComponent(driver, "Type").getValue(),
-                "Customer"
-        );
-        Assert.assertEquals(
-                new AccountDetailsComponent(driver, "Industry").getValue(),
-                "Banking"
-        );
-    }
-
-    public AccountsSteps createNewAccount(AccountModel accountModel) {
-        accountPage = new AccountPage(driver);
-        accountPage.openNewAccountForm();
-        CreateAccountFormComponent form = new CreateAccountFormComponent(driver);
-        Assert.assertTrue(
-                form.isComponentDisplayed(),
-                form.getClass().getSimpleName().concat(" not displayed")
-        );
-        fillAccountForm(accountModel);
-        form.save();
-        validatePageIsLoaded(accountPage);
-        return this;
-    }*/
-
-    private void fillAccountForm(AccountModel accountModel) {
-        Input accountNameInput = new Input(driver, "Account Name");
-        accountNameInput.insert(accountModel.getAccountName());
-        Input phoneInput = new Input(driver, "Phone");
-        phoneInput.insert(accountModel.getPhone());
-        new Input(driver, "Website").insert(accountModel.getWebsite());
-        new Input(driver, "Employees").insert(String.valueOf(accountModel.getEmployees()));
-        new Dropdown(driver, "Type").selectOption(accountModel.getType());
-        new Dropdown(driver, "Industry").selectOption(accountModel.getIndustry());
-    }
-
-    public void validateAccountCreated(AccountModel expectedModel) {
-        accountPage.openDetails();
-        AccountModel actualModel = getActualAccount();
-        Assert.assertEquals(
-                actualModel,
-                expectedModel,
-                String.format("Account is not valid: %s", actualModel)
-        );
-    }
-
-    private AccountModel getActualAccount( ) {
-        AccountModel actualModel = new AccountModel();
-        actualModel.setAccountName(new AccountDetailsComponent(driver, "Account Name", "text").getValue());
-        actualModel.setPhone(new AccountDetailsComponent(driver, "Phone", "phone").getValue());
-        actualModel.setWebsite(new AccountDetailsComponent(driver, "Website", "url").getValue());
-        actualModel.setEmployees(Integer.parseInt(new AccountDetailsComponent(driver, "Employees", "number").getValue()));
-        actualModel.setType(new AccountDetailsComponent(driver, "Type", "text").getValue());
-        actualModel.setIndustry(new AccountDetailsComponent(driver, "Industry", "text").getValue());
-        return actualModel;
     }
 
 }
